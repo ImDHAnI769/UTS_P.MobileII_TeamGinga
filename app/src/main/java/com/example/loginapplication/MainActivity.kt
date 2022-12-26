ceb51460e9fdcdd36d687bf1f50176b51421ea48
Bima Rahmadhani <bimaramadhani795@gmail.com>
2022-12-25 21:04:58 -0800

    perbaikan program

--- app/src/main/java/com/example/loginapplication/MainActivity.kt
+++ app/src/main/java/com/example/loginapplication/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.loginapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Icon
-import androidx.compose.runtime.*
-import androidx.compose.ui.res.painterResource
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.loginapplication.composable.*
+import com.example.loginapplication.navigation.NavGraph
 import com.example.loginapplication.ui.theme.LoginApplicationTheme
 
 class MainActivity : ComponentActivity() {
